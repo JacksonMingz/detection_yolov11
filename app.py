2037f480ff5f8d6b39a179d5a45e20d0f6009046
JacksonMingz <jacksonmingz@gmail.com>
2025-08-16 15:29:03 +0700

    clean

--- app.py
+++ app.py
@@ -3,15 +3,12 @@ from flask_cors import CORS
 from ultralytics import YOLO
 import cv2
 import numpy as np
-import tempfile
-import os
 import base64
 
 app = Flask(__name__)
 
 CORS(app)
 
-# Load model sekali di awal
 model = YOLO("./best.pt")
 
 @app.route('/detect', methods=['POST'])
@@ -20,20 +17,15 @@ def detect():
         return {'error': 'No image uploaded'}, 400
 
     file = request.files['image']
-    # Baca file gambar ke numpy array
     file_bytes = np.frombuffer(file.read(), np.uint8)
     img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
 
-    # Inference
     results = model(img)
-    # Render hasil deteksi ke gambar numpy array
     result_img = results[0].plot()
 
-    # Encode image to base64
     _, buffer = cv2.imencode('.jpg', result_img)
     img_base64 = base64.b64encode(buffer).decode('utf-8')
 
-    # Extract detection results
     dets = []
     names = results[0].names
     for box in results[0].boxes:
